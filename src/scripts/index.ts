de3d472f51708ff08ba75b2f53bfaa79bac1c2ee
David Robles <drobles@gmail.com>
2021-09-26 08:59:16 -0700

    update

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,9 +1,40 @@
-// /**/const world = 'world';
+
+class GridWorld {
+
+    nRows: number;
+    nCols: number;
+
+    constructor(nRows: number, nCols: number) {
+        this.nRows = nRows;
+        this.nCols = nCols;
+    }
+
+}
+
+class GridWorldView {
+
+    gw: GridWorld;
+    el: HTMLElement;
+
+    constructor(gw: GridWorld, el: HTMLElement) {
+        this.gw = gw;
+        this.el = el;
+    }
+
+    render() {
+        this.el.innerHTML = "<p>yeah</p>"
+    }
+
+}
 
 export function hello(world: string = 'world'): string {
     console.log("hello world");
     return `Hello ${world}! `;
 }
 
-hello("yes");
-hello("yes");
+const gw = new GridWorld(20, 20)
+const el = document.getElementById("gw")
+if (el) {
+    const gwView = new GridWorldView(gw, el);
+    gwView.render()
+}
